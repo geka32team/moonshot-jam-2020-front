030d4d1a2308ad5e0264b815f337d85fa9c379b3
Vasyl Tsyva <sodiicc@gmail.com>
2020-11-28 18:33:47 +0200

    add debug info

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -12,6 +12,7 @@ function Registration(props) {
   const [confPassword, setConfPassword] = useState('')
 
   let url = process.env.REACT_APP_API_URL || 'http://127.0.0.1:5000/api'
+  
   console.log('process.env.REACT_APP_API_URL', process.env.REACT_APP_API_URL)
   console.log('process.env.API_URL', process.env.API_URL)
 
