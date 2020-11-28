bb5e21f88eddbef37ca7a4b55b7cf69df6d872d5
Vasyl Tsyva <sodiicc@gmail.com>
2020-11-28 15:16:38 +0200

    add socket

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ export default function Header(props) {
     <StyledField>
 
       <header>
-        <audio src={moon_sound} autoPlay loop />
+        {/* <audio src={moon_sound} autoPlay loop /> */}
         <div className="header-wrapper">
           <div className="nick-name"><span className='lvl'>{props.info.lvl}</span>{props.info.name}</div>
           <div className="game-name">MoonnyMathics</div>
