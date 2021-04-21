d58f41458b903ed9dfd980be36da31c21bc24092
Vasyl Tsyva <sodiicc@gmail.com>
2021-04-21 16:02:56 +0300

    included server here

---
+++ server/query.js
@@ -0,0 +1,43 @@
+exports.create_task =
+  "INSERT INTO public.tasks(value, result, character_nickname, created) VALUES ($1, $2, $3, $4)";
+
+exports.set_character =
+  "UPDATE public.character SET lvl =$1, free_stats = $2, current_hp = $3, exp = $4, current_exp = $5 WHERE nickname = $6;";
+
+exports.set_bot =
+  "UPDATE public.bot SET nickname=$1, lvl=$2, hp=$3, current_hp=$4, str=$5, vit=$6, dex=$7, acc=$8, dmg=$9, date=$10, diff=$11 WHERE attacker_nickname = $12;";
+
+exports.set_bot_hp =
+  "UPDATE public.bot SET current_hp = ($1) WHERE attacker_nickname = ($2);";
+
+exports.set_hp_exp =
+  "UPDATE public.character SET current_hp = ($1), current_exp = ($2), free_stats = ($3) WHERE nickname = ($4);";
+
+exports.set_str =
+  "UPDATE public.character SET str = str + 1, free_stats = free_stats - 1 WHERE nickname = ($1);";
+
+exports.set_vit =
+  "UPDATE public.character SET vit = vit + 1, free_stats = free_stats - 1, hp = vit * 4 + 54, current_hp = vit * 4 + 54  WHERE nickname = ($1);";
+
+exports.set_dex =
+  "UPDATE public.character SET dex = dex + 1, free_stats = free_stats - 1 WHERE nickname = ($1);";
+
+exports.set_acc =
+  "UPDATE public.character SET acc = acc + 1, free_stats = free_stats - 1 WHERE nickname = ($1);";
+
+exports.get_stat =
+  "SELECT free_stats FROM public.character WHERE nickname = ($1);";
+
+exports.get_char_info = "SELECT * FROM public.character WHERE nickname = ($1);";
+
+exports.get_bot_info =
+  "SELECT * FROM public.bot WHERE attacker_nickname = ($1);";
+
+exports.get_task =
+  "SELECT result, created FROM public.tasks WHERE character_nickname = ($1) ORDER BY created DESC LIMIT 1;";
+
+exports.get_current_bot_info =
+  "SELECT * FROM public.bot WHERE attacker_nickname = ($1) ORDER BY date DESC LIMIT 1;";
+
+exports.get_result =
+  "SELECT result, created FROM public.tasks WHERE character_nickname = ($1) ORDER BY created DESC LIMIT 1;";
---
+++ server/server.js
@@ -0,0 +1,236 @@
+const express = require("express");
+const cors = require("cors");
+const { Pool } = require("pg");
+const tasks = require("./tasks");
+const query = require("./query");
+const battle_calc = require("./battle_calc");
+const exp_calc = require("./exp_calc");
+const bot_generator = require("./bot_generator");
+
+const port = 9000;
+const app = express();
+app.use(cors());
+
+// const client = new Client({
+//   user: "postgres",
+//   host: "localhost",
+//   database: "MoonnyMathics",
+//   password: "12345678",
+//   port: 5432,
+// });
+
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "MoonnyMathics",
+  password: "12345678",
+  port: 5432,
+});
+// client.connect();
+
+app.get("/character/:name", (req, res) => {
+  pool.query(query.get_char_info, [req.params.name], (err, response) => {
+    res.send(response.rows);
+  });
+});
+
+app.get("/getexp", (req, res) => {
+  const lvl = req.query.lvl;
+  const bot_lvl = req.query.botlvl;
+  const diff = req.query.diff;
+  const exp = exp_calc.get_real_exp(lvl, bot_lvl, diff) + ''
+  res.send([exp]);
+});
+
+app.get("/bot/:name", (req, res) => {
+  pool.query(query.get_bot_info, [req.params.name], (err, response) => {
+    res.send(response.rows);
+  });
+});
+
+app.get("/stats", (req, res) => {
+  const stat_name = req.query.stat;
+  const nickname = req.query.nickname;
+
+  pool.query(query.get_stat, [nickname], (err, response) => {
+    const { free_stats } = response.rows[0];
+    if (err) console.log(err);
+    else res.send([200]);
+
+    if (free_stats > 0) pool.query(query[`set_${stat_name}`], [nickname]);
+  });
+});
+
+app.get("/newbot", (req, res) => {
+  const diff = +req.query.diff;
+  const bot_lvl = +req.query.lvl;
+  const nickname = req.query.nickname;
+
+  let gen_bot = bot_generator.bot(bot_lvl, diff, nickname, "Test_bot");
+  const params = [
+    gen_bot.nickname,
+    gen_bot.lvl,
+    gen_bot.hp,
+    gen_bot.current_hp,
+    gen_bot.str,
+    gen_bot.vit,
+    gen_bot.dex,
+    gen_bot.acc,
+    gen_bot.dmg,
+    gen_bot.date,
+    gen_bot.diff,
+    gen_bot.attacker_nickname,
+  ];
+
+  pool.query(query.set_bot, params, () => {
+
+    res.send([200]);
+  })
+
+});
+
+app.get("/task", (req, res) => {
+  const diff = req.query.diff;
+  const bot_lvl = req.query.lvl;
+  const nickname = req.query.nickname;
+  const [value, result] = tasks.difficulty[diff](bot_lvl);
+
+  pool.query(query.create_task, [value, result, nickname, Date.now()]);
+
+  res.send([value, result.length]);
+});
+
+app.get("/battleresult/:name", (req, res) => {
+  pool.query(query.get_char_info, [req.params.name], (err, response) => {
+    const {
+      hp,
+      exp,
+      current_exp,
+      current_hp,
+      lvl,
+      free_stats,
+      bosses_defeat,
+    } = response.rows[0];
+    pool.query(query.set_hp_exp, [
+      hp,
+      current_exp,
+      free_stats,
+      req.params.name,
+    ]);
+    if (err) res.send(err);
+    else res.send([200]);
+  });
+});
+
+app.get("/answer", (req, res) => {
+  const value = req.query.value;
+  const nickname = req.query.nickname;
+  let char;
+  let enemy;
+  let task_params;
+
+  pool.query(query.get_result, [nickname], (err, response) => {
+    task_params = response.rows[0];
+    let result = task_params.result;
+    let created = task_params.created;
+
+    pool.query(query.get_char_info, [nickname], (err, response) => {
+      char = response.rows[0];
+
+      pool.query(query.get_current_bot_info, [nickname], (err, response) => {
+        enemy = response.rows[0];
+        let [char_hit, is_char_crit] = battle_calc.calc(
+          char.lvl,
+          char.dmg,
+          char.dex,
+          char.str,
+          char.acc,
+          char.vit,
+          enemy.lvl,
+          enemy.str,
+          enemy.acc,
+          (Date.now() - created) / 1000,
+          char.time,
+          true
+        );
+        const [enemy_hit, is_enemy_crit] = battle_calc.calc(
+          enemy.lvl,
+          enemy.dmg,
+          enemy.dex,
+          enemy.str,
+          enemy.acc,
+          enemy.vit,
+          char.lvl,
+          char.str,
+          char.acc
+        );
+
+        if (value != result) char_hit = 0;
+        let hp_left = char.current_hp - enemy_hit;
+        let enemy_hp_left = enemy.current_hp - char_hit;
+        is_end = hp_left < 1 || enemy_hp_left < 1;
+
+        let result_fight = null;
+        if (is_end)
+          result_fight =
+            hp_left > enemy_hp_left
+              ? "win"
+              : hp_left < enemy_hp_left
+              ? "lose"
+              : "draw";
+        let exp =
+          result_fight == "win"
+            ? exp_calc.get_real_exp(char.lvl, enemy.lvl, enemy.diff)
+            : 0;
+
+        let ret = {
+          char_hit,
+          enemy_hit,
+          drop: [],
+          is_end,
+          result_fight,
+          is_char_crit,
+          is_enemy_crit,
+          exp,
+        };
+
+        let set_current_exp = char.current_exp + exp;
+        let set_exp = char.exp;
+        let set_lvl = char.lvl;
+        let set_free_stats = char.free_stats;
+        if (set_current_exp >= exp_calc.get_level_exp(char.lvl)) {
+          set_lvl += 1;
+          set_free_stats += 5;
+          set_exp = exp_calc.get_level_exp(set_lvl);
+          set_current_exp -= char.exp;
+        }
+
+        pool.query(
+          query.set_character,
+          [
+            set_lvl,
+            set_free_stats,
+            hp_left,
+            set_exp,
+            set_current_exp,
+            "Sodiicc",
+          ],
+          (err, response) => {
+            pool.query(
+              query.set_bot_hp,
+              [enemy_hp_left, "Sodiicc"],
+              (err, response) => {
+                console.log("err", err);
+                res.send(ret);
+              }
+            );
+          }
+        );
+      });
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
